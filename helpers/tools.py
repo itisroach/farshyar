ea131ecaf91ca76d33d67f411a2913cd74a7a10f
unknown <rouchashoori@gmail.com>
2025-02-21 20:34:51 +0330

    add function that can seprate data from text that extracted from message

--- helpers/tools.py
+++ helpers/tools.py
@@ -9,3 +9,25 @@ def ReadEnvVar(name: str):
 
     return value
 
+
+def Create_Data(extractedWords: list[str]):
+
+    data = {
+        "title": "",
+        "details": "",
+        "quantity": 0
+    }
+
+    for word in extractedWords:
+        if "فرش" in word:
+            data["title"] = word
+        
+        
+        elif "تخته" in word:
+            data["quantity"] = int(word.replace("تخته", ""))
+
+        else:
+            data["details"] += word
+
+
+    print(data)
