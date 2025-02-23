5231e93cc5b7c3ab0d36d5df31c4bec44a197735
unknown <rouchashoori@gmail.com>
2025-02-23 19:11:12 +0330

    add some functions to help fetch and insert data

--- db/main.py
+++ db/main.py
@@ -1,12 +1,12 @@
 from helpers import tools
 import asyncpg
 import sys
-import queries
+from . import queries
 
 DBConfig = {
-    "user": tools.ReadEnvVar("USER_DB"),
-    "password": tools.ReadEnvVar("PASS_DB"),
-    "host": tools.ReadEnvVar("HOST_DB"),
+    "user": tools.ReadEnvVar("DB_USER"),
+    "password": tools.ReadEnvVar("DB_PASS"),
+    "host": tools.ReadEnvVar("DB_HOST"),
     "database": tools.ReadEnvVar("DB_NAME"),
     "port": tools.ReadEnvVar("DB_PORT"),
     "server_settings": {"client_encoding": "UTF8"}
@@ -15,23 +15,37 @@ DBConfig = {
 
 class Database():
     _instance = None
+    _initialized = False  # Prevent duplicate init
 
-
-    def __init__(self):
-        if self._instance is None:
-            self._instance = super(Database, self).__new__(self)
-    
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Database, cls).__new__(cls)
+            cls._instance.pool = None
+            
+        return cls._instance
 
     async def init_db(self):
+        # Initialize the database pool if it hasn't been initialized yet.
+        if self._initialized:
+            return  # Prevent re-initialization
         try:
             self.pool = await asyncpg.create_pool(**DBConfig)
-        except Exception as e:
-            sys.exit(e)
+            self._initialized = True
 
-        # creating table on database connection
+        except Exception as e:
+            sys.exit(f"Database connection failed: {e}")
+
+        # Creating table on database connection
         await self.pool.execute(queries.create_table_query)
 
-
     async def fetch_products(self):
+        # Fetch products from database
+        if self.pool is None:
+            raise RuntimeError("Database not initialized. Call init_db() first.")
         async with self.pool.acquire() as connection:
             return await connection.fetch(queries.fetch_items_query)
+        
+    async def add_products(self, *args):
+        
+        async with self.pool.acquire() as connection:
+            return await connection.fetch(queries.insert_item_query, *args)
