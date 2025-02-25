56d1b362dff2d43bb9917108e2a96112f7e207af
unknown <rouchashoori@gmail.com>
2025-02-25 10:35:16 +0330

    fix bugs

--- helpers/messageParser.py
+++ helpers/messageParser.py
@@ -1,6 +1,7 @@
 import re
 from . import tools
 
+# finding matches of important information that we need
 def ParseMessage(message: str):
     pattern = r"(\d{4}\s?شانه|\d{3}\s?شانه|\d+\s?ش|\d{1,2}\s?ت|[\d]+(?:\s?متر|متری|متراژ)?|کد\s?\d+|\d+)"
     title_pattern = r"^(.*)"
@@ -9,12 +10,10 @@ def ParseMessage(message: str):
     foundTitle = re.findall(title_pattern, message)
 
 
-    print("matches",matches)
-
     matches.append(foundTitle[0])
 
+    print("matches",matches)
     
-    data = tools.Create_Data(matches)
 
-    return data
+    return matches
     
