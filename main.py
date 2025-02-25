2e158267f1d3b2e3389c37a56c252bcd6301946a
unknown <rouchashoori@gmail.com>
2025-02-25 10:33:56 +0330

    add image proccessing in the background and the bot now can upload images to cloud

--- main.py
+++ main.py
@@ -3,16 +3,21 @@ from helpers import tools
 from db.main import Database
 from helpers import messageParser
 import asyncio
+from helpers.cloudStorage import ProcessImages
 
+# telegram credentials
 API_ID    = tools.ReadEnvVar("API_ID")
 API_HASH  = tools.ReadEnvVar("API_HASH")
 BOT_TOKEN = tools.ReadEnvVar("BOT_TOKEN")
 SESSION_NAME = tools.ReadEnvVar("SESSION_NAME")
 
+# an instance of database connection
 db = Database()
 
 async def main():
-    client = await TelegramClient(session=SESSION_NAME, api_id=API_ID, api_hash=API_HASH).start(bot_token=BOT_TOKEN)
+    # an instance of telegram client
+    client = TelegramClient(session=SESSION_NAME, api_id=API_ID, api_hash=API_HASH)
+
 
     async with client:
 
@@ -20,12 +25,37 @@ async def main():
 
         @client.on(events.NewMessage())
         async def ListenForMessages(event):
+            # if message contains mutiple images goes out of function
+            if event.message.grouped_id:
+                return
+
+
+            # checks if message contains text
             if event.text:
-                data = messageParser.ParseMessage(event.text)
-                # add products to database
-                await db.add_products(*data.values())
+                # geetting  matched from the text
+                matches = messageParser.ParseMessage(event.text)
 
+                # checks if message contains only one image 
+                if event.message.photo:
+                    # runs this procces in background
+                    asyncio.create_task(ProcessImages(event, client, matches, db, False))
 
+                # add products to database if we don't have image
+                else:
+                    await tools.Create_Data(matches, None, db)
+
+        # this event happens when multiple images sent
+        @client.on(events.Album())
+        async def ListenForImages(event):
+            # check if message contains text
+           if event.text:
+                # finding matches in text
+                matches = messageParser.ParseMessage(event.text)
+                # uploading file to cloud in the background
+                asyncio.create_task(ProcessImages(event, client, matches, db, True))
+
+                
+    # starting the bot
     await client.start()
     await client.run_until_disconnected()
 
