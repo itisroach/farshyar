a5b578efe682dd29aeb4aeae78fddb8f03cc036a
unknown <rouchashoori@gmail.com>
2025-02-25 10:34:48 +0330

    improved the logic of extracting information from the text

--- helpers/tools.py
+++ helpers/tools.py
@@ -5,6 +5,12 @@ import re
 
 load_dotenv()
 
+def ReadEnvVar(name: str):
+    value = os.getenv(name)
+
+    return value
+
+
 # a data type that can generate a list of tuples that can store tuples like this (size, quantity) this is for knowing how many stocks we have in each size
 class Sizes():
     _instance = None
@@ -37,38 +43,46 @@ class Sizes():
     def destroy(self):
         self._instance.listOfSizes = []
 
-def ReadEnvVar(name: str):
-    value = os.getenv(name)
 
-    return value
 
 
 # this simply returns a python dict containing products information that is represented for database fields 
-def Create_Data(extractedWords: list[str]):
+async def Create_Data(extractedWords: list[str], images: list[str], db):
 
     data = {
         "title": extractedWords[-1],
         "details": "",
         "sizes": [],
         "comb": 0,
-        "post_link": "",
-        "post_id": ""
+        "post_link": "test",
+        "post_id": "test",
+        "images": images
     }
     size = Sizes()
-    for word in extractedWords:
-        data["title"] = extractedWords[-1]
+
+    for idx, word in enumerate(extractedWords):
+        
+        # checking if the word that it reaches is not the title and if it is not the title it removes every other match in the title 
+        if idx != len(extractedWords)-1:
+            extractedWords[-1] = extractedWords[-1].replace(word, "") 
+            data["title"] = extractedWords[-1].strip().replace(" ", " ")
+
         # if theres code in title it will remove it
         if "کد" in word:    
             data["title"] = re.sub(r"کد\s+\S+", "", extractedWords[-1]).strip()
-            data["title"] = data["title"].replace("  " , "")
+            data["title"] = data["title"].replace("  " , " ")
 
-        elif "تخته" in word or re.match(r"(?<!\S)\d*ت|\dت(?!\S)", word):
-            quantity = word.replace("تخته", "")
-            quantity = re.sub(r'(\d)[\u0600-\u06FF]', r'\1', word)
+        # checks if the نخته is in the match and if it is it will be considered as quantity in Sizes class
+        elif "تخته" in word or re.search(r"(?<!\S)[\d\u06F0-\u06F9]*ت|[\d\u06F0-\u06F9]ت(?!\S)", word) is not None:
+            if "تخته" in word:
+                quantity = word.replace("تخته", "")
+            else:
+                quantity = word.replace("ت", "")
 
             size.add_data(int(quantity), 1)
         
-        elif "شانه" in word or re.match(r"(?<!\S)\d*ش|\dش(?!\S)"):
+        # checks if شانه is in the match and if it is it will clean it and consider it as comb
+        elif "شانه" in word or re.search(r"(?<!\S)[\d\u06F0-\u06F9]*ش|[\d\u06F0-\u06F9]ش(?!\S)", word) is not None or word in ["1500", "1000", "700", "1200", "۱۲۰۰", "۱۰۰۰", "۷۰۰", "۱۵۰۰"]:
             if "شانه" in word:
                 comb = word.replace("شانه", "")
             else:
@@ -76,15 +90,21 @@ def Create_Data(extractedWords: list[str]):
 
             data["comb"] = int(comb)
 
-        
+        # detecting the actual size
         elif word in ["4", "6", "9", "12", "۱۲", "۴", "۹", "۶"] or "متر" in word:
-            word = word.replace("متر", "")
-            length = int(word)
+            length = int(word.replace("متر", ""))
             size.add_data(length, 0)
 
 
+    # convert the Size class output to json so it can be stored in database
     data["sizes"] = (json.dumps(size.get_sizes()))
 
+    # destory the class intance for further use to bed intialized
     size.destroy()
 
-    return data
+    print(data)
+    
+    # inserting product to database
+    await db.add_products(*data.values())
+
+    
