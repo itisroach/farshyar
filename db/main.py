bd7db27b7cf9f9006906ef31f64f3012d57dc9fb
unknown <rouchashoori@gmail.com>
2025-02-28 22:14:03 +0330

    changed database back to postgresql "

--- db/main.py
+++ db/main.py
@@ -1,9 +1,16 @@
-import motor.motor_asyncio
 from helpers import utils
-import motor
+import asyncpg
 import sys
+from . import queries
 
-MONGODB_URI=utils.ReadEnvVar("MONGO_DB_URI")
+DBConfig = {
+    "user": utils.ReadEnvVar("DB_USER"),
+    "password": utils.ReadEnvVar("DB_PASS"),
+    "host": utils.ReadEnvVar("DB_HOST"),
+    "database": utils.ReadEnvVar("DB_NAME"),
+    "port": utils.ReadEnvVar("DB_PORT"),
+    "server_settings": {"client_encoding": "UTF8"}
+}
 
 
 class Database():
@@ -14,8 +21,6 @@ class Database():
         if cls._instance is None:
             cls._instance = super(Database, cls).__new__(cls)
             cls._instance.pool = None
-            cls._instance.db   = None
-            cls._instance.collection = None
             
         return cls._instance
 
@@ -24,51 +29,37 @@ class Database():
         if self._initialized:
             return  # Prevent re-initialization
         try:
-            self.pool = motor.motor_asyncio.AsyncIOMotorClient()
-            self.db   = self.pool.farshyab
-            self.collection = self.db.products
+            self.pool = await asyncpg.create_pool(**DBConfig)
             self._initialized = True
 
         except Exception as e:
             sys.exit(f"Database connection failed: {e}")
 
-        # # Creating table on database connection
-        # await self.pool.execute(queries.create_table_query)
+        # Creating table on database connection
+        await self.pool.execute(queries.create_table_query)
 
     async def fetch_products(self):
+        # Fetch products from database
+        if self.pool is None:
+            raise RuntimeError("Database not initialized. Call init_db() first.")
+        async with self.pool.acquire() as connection:
+            return await connection.fetch(queries.fetch_items_query)
         
-        return await self.collection.find()
-
-    async def add_products(self, data):
+    async def add_products(self, *args):
         
-        await self.collection.insert_one(data)
+        async with self.pool.acquire() as connection:
+            return await connection.execute(queries.insert_item_query, *args)
         
 
-    async def update_products(self, channel_id, message_id, data):
-
-        result = await self.collection.find_one_and_update(
-            filter={"post_id": message_id, "channel_id": channel_id},
-            update={"$set": data},
-            projection={"channel_posts_id": 1}
-        )
-
-
-        return result["channel_posts_id"]
-
+    async def update_products(self, channel_id, message_id, *args):
+        async with self.pool.acquire() as connection:
+            return await connection.fetch(queries.update_item_query, *args, channel_id, message_id)
         
     async def delete_product(self, channel_id, post_id):
-
-        result = await self.collection.find_one_and_delete(
-            filter={"channel_id": channel_id, "post_id": post_id},
-            projection={"images":1}
-        )
-
-        return result
+        async with self.pool.acquire() as connection:
+            return await connection.fetch(queries.delete_item_query, channel_id, post_id) 
         
 
     async def fetch_products_to_remove(self):
-
-
-        result = self.collection.find({}, {"channel_id":1 , "post_id": 1, "channel_posts_id": 1})
-
-        return await result.to_list()
+        async with self.pool.acquire() as connection:
+            return await connection.fetch(queries.fetch_items_to_remove)
---
+++ db/queries.py
@@ -0,0 +1,63 @@
+create_table_query = """
+    CREATE TABLE IF NOT EXISTS products (
+        id               SERIAL PRIMARY KEY,
+        title            VARCHAR(256) NOT NULL,
+        sizes            JSONB NOT NULL,
+        comb             INT NOT NULL,
+        details          TEXT,
+        post_link        TEXT NOT NULL,
+        post_id          TEXT NOT NULL,
+        channel_id       TEXT NOT NULL,
+        images           TEXT[],
+        channel_posts_id INT[],
+        created_at       TIMESTAMP DEFAULT current_timestamp
+    )
+"""
+
+create_images_table_query = """
+    CREATE TABLE IF NOT EXISTS images (
+        id       SERIAL PRIMARY KEY,
+        url      TEXT NOT NULL,
+        product  INT NOT NULL,
+        CONSTRAINT fk_product FOREIGN KEY(product) REFERENCES products(id)
+    )
+"""
+
+# the sizes field represent the size and quantity of each size like this [(size, quantity), ...]
+insert_item_query = """
+    INSERT INTO products (title, details, sizes, comb, post_link, post_id, channel_id, images, channel_posts_id) VALUES ($1, $2, $3::jsonb, $4, $5, $6, $7, $8, $9)
+"""
+
+
+fetch_items_to_remove = """
+    SELECT channel_id, post_id, channel_posts_id FROM products
+"""
+
+delete_item_query = """
+    DELETE FROM products WHERE channel_id = $1 AND post_id = $2 RETURNING images
+"""
+
+
+update_item_query = """
+    UPDATE products SET 
+    title=$1, details=$2, sizes=$3, comb=$4
+    WHERE channel_id = $5 AND post_id = $6
+    RETURNING channel_posts_id
+"""
+
+
+fetch_items_query = """
+    SELECT 
+        id,
+        title, 
+        sizes, 
+        comb, 
+        details, 
+        post_link, 
+        post_id, 
+        channel_id,
+        images,
+        TO_CHAR(created_at, 'YYYY/MM/DD HH:MM:SS') AS created_at 
+
+    FROM products
+"""
